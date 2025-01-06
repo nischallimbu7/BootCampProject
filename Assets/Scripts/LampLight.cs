79edd5176ae31ffbca5a37cdcbc4001529301ab2
MonicheStedford <152186068+cerose88@users.noreply.github.com>
2025-01-06 17:05:26 +0000

    Update LampLight.cs

---
+++ Assets/Scripts/LampLight.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class LampLight : MonoBehaviour
+{
+    public Light lamp;
+    public float startTime, reduceTimer, reductionSpeed, maxIntensity;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        lamp.intensity = maxIntensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lamp.intensity >0)
+        {
+            lamp.intensity -= Time.deltaTime * reductionSpeed;
+        }
+
+       
+        
+    }
+
+   
+
+}

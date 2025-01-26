9140e28f7ab9e32a0959b7d1611c7c42bc202462
Miscaslt <148296090+Miscaslt@users.noreply.github.com>
2025-01-26 15:30:13 +0000

    level design & enemy behaviour
    added a collision sphere as detection range

--- Assets/Scripts/Enemy/MonsterGrowl.cs
+++ Assets/Scripts/Enemy/MonsterGrowl.cs
@@ -25,7 +25,7 @@ public class MonsterGrowl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        growlingAudio.clip = clip[audioIndex];
+      //  growlingAudio.clip = clip[audioIndex];
         // StartCoroutine("Growl");
         
             //growlingAudio.enabled = true;
